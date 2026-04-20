793387fb5f52fe46e46b148f993f8ad29185970b
DaniusKalv <danius@kurokesu.com>
2026-04-21 00:24:36 +0300

    Rewrite cinepi.service around tty1, drop helpers

--- deploy/cinepi-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Tell systemd to stop printing status messages to console
-kill -SIGRTMIN+21 1 2>/dev/null
-# Clear ttys that might have content
-printf '\033[2J\033[H' > /dev/tty7 2>/dev/null
-# Switch to unused tty (tty12 has kernel console, tty7 had Cage)
-/usr/bin/chvt 11
