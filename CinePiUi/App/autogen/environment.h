d0aa4ba646eaf99de365a34360ae708d397144e2
DaniusKalv <danius@kurokesu.com>
2026-05-07 11:59:31 +0300

    Drop stale env.h imports for removed QDS modules

--- CinePiUi/App/autogen/environment.h
+++ CinePiUi/App/autogen/environment.h
@@ -15,12 +15,7 @@ const char mainQmlFile[] = "qrc:/qt/qml/CinePiUiContent/App.qml";
 
 #ifdef BUILD_QDS_COMPONENTS
 
-Q_IMPORT_QML_PLUGIN(QtQuick_Studio_ComponentsPlugin)
-Q_IMPORT_QML_PLUGIN(QtQuick_Studio_EffectsPlugin)
 Q_IMPORT_QML_PLUGIN(QtQuick_Studio_ApplicationPlugin)
-Q_IMPORT_QML_PLUGIN(FlowViewPlugin)
-Q_IMPORT_QML_PLUGIN(QtQuick_Studio_LogicHelperPlugin)
-Q_IMPORT_QML_PLUGIN(QtQuick_Studio_MultiTextPlugin)
 Q_IMPORT_QML_PLUGIN(QtQuick_Studio_EventSimulatorPlugin)
 Q_IMPORT_QML_PLUGIN(QtQuick_Studio_EventSystemPlugin)
 
